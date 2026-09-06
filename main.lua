387a354caf10c49d35fd69c959130cda164c7795
TheApproachingStorm <peterparkerbongs@gmail.com>
2026-09-06 08:44:01 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,5 @@
 print("dqr_bot loaded!")
 
---------------------------------------------------
--- Load modules
---------------------------------------------------
-
 local Utils = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/TheApproachingStorm/dqr_bot/main/utils.lua?t=" .. os.time()
 ))()
@@ -12,52 +8,28 @@ local Methods = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/TheApproachingStorm/dqr_bot/main/methods.lua?t=" .. os.time()
 ))()
 
---------------------------------------------------
--- Place check
---------------------------------------------------
-
 if game.PlaceId ~= 85776757589518 then
     return
 end
 
 print("Correct place detected.")
 
---------------------------------------------------
--- Start dungeon
---------------------------------------------------
-
 Utils.StartDungeon()
-
---------------------------------------------------
--- Air suspend
---------------------------------------------------
 
 Methods.AirSuspend()
 
---------------------------------------------------
--- Locate -> Tween -> Wait -> Repeat
---------------------------------------------------
-
 while true do
-
     local clusters = Methods.LocateEnemies()
 
     if #clusters > 0 then
-
         for _, cluster in ipairs(clusters) do
-
             local tween = Methods.Tween(cluster.center)
 
             tween.Completed:Wait()
 
             task.wait(6)
-
         end
-
     else
-
         task.wait(1)
-
     end
-
 end
