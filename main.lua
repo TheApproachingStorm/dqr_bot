932e77c46eace57a11ec7667ebbdd74244c9dc3a
TheApproachingStorm <peterparkerbongs@gmail.com>
2026-09-11 00:43:39 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,40 +1,72 @@
-print("dqr_bot v5!")
+repeat task.wait() until game:IsLoaded()
 
-local cacheBust = tostring(os.time())
+-- Wait until the player is actually spawned
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
 
-local Utils = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/TheApproachingStorm/dqr_bot/main/utils.lua?t=" .. cacheBust
-))()
+repeat
+    task.wait()
+until player.Character
+    and player.Character:FindFirstChild("Humanoid")
+    and player.Character:FindFirstChild("HumanoidRootPart")
 
-local Methods = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/TheApproachingStorm/dqr_bot/main/methods.lua?t=" .. cacheBust
-))()
+print("character spawned.")
 
-if game.PlaceId ~= 85776757589518 then
-    return
-end
+warn("dqr_bot v9!")
+    -- load modules
+    local Utils = loadstring(readfile("dqr_bot-main/utils.lua"))()
+    local Methods = loadstring(readfile("dqr_bot-main/methods.lua"))()
 
-print("Correct place detected.")
+if game.PlaceId == 85776757589518 then -- dungeon
 
-Utils.StartDungeon()
+    print("dungeon detected.")
 
-task.wait(6)
+    -- start dungeon
+    Methods.DeathRetry(Utils)
+    Utils.StartDungeon()
+    Utils.RemoveMap()
+    task.wait(6)
 
-Utils.Noclip()
-Methods.AirSuspend()
+    -- begin tracking the rooms
+    task.spawn(function()
+        Utils.RoomTracker(Methods, function()
+            print("tracking room..")
+        end)
+    end)
 
-while true do
-    local clusters = Methods.LocateEnemies()
+    -- Initialize loot listener ONCE
+    Utils.LootListener(Methods)
 
-    if #clusters > 0 then
-        for _, cluster in ipairs(clusters) do
-            local walk = Methods.WalkTo(cluster.center)
+    task.wait(1)
+    -- Start autofarm path
+    print("starting farm!")
+    Methods.PathFind(Utils)
+    print("dungeon finished..")
 
-            walk.Completed:Wait()
+elseif game.PlaceId == 77649408247578 then -- lobby
 
-            task.wait(6)
-        end
-    else
-        task.wait(1)
-    end
+    print("lobby detected.")
+
+    -- play game
+    Utils.PlayGame()
+
+    local playerGui = player:WaitForChild("PlayerGui")
+    local mainInterface = playerGui:WaitForChild("mainInterface")
+
+    -- Wait until the game puts us back into the menu
+    repeat
+        task.wait()
+    until not mainInterface.Enabled
+
+    print("menu detected. playing again.")
+    -- play game 2nd time
+    Utils.PlayGame()
+
+    print("queing up")
+
+    -- start dungeon 
+    Utils.QueueUp()
+    task.wait(1)
+    Utils.StartQueue()
+
 end
